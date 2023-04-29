b9a04d58aebce25a85cc5407c308a5903fbe3f1d
Syota Nishimura <syota4723@gmail.com>
2023-04-30 00:24:24 +0900

    :lipstick: Sidebarコンポーネントの雛形作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
     </div>
   );
 }
