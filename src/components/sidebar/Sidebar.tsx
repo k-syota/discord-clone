a687faab3b59bd19b8351486642b09c767f3e42e
Syota Nishimura <syota4723@gmail.com>
2023-05-01 00:17:11 +0900

    :lipstick: Sidebarのフッターデザインを作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import './Sidebar.scss'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import SidebarChannel from './SidebarChannel';
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Sidebar = () => {
     return (
@@ -34,6 +37,20 @@ const Sidebar = () => {
                     <SidebarChannel />
                     <SidebarChannel />
                     <SidebarChannel />
+                </div>
+                <div className="sidebarFooter">
+                    <div className="sidebarAccount">
+                        <img src="./icon.jpg" alt="" />
+                        <div className="accountName">
+                            <h4>kanon</h4>
+                            <span>#4723</span>
+                        </div>
+                    </div>
+                    <div className="sidebarVoice">
+                        <MicIcon />
+                        <HeadphonesIcon />
+                        <SettingsIcon />
+                    </div>
                 </div>
             </div>
         </div>
