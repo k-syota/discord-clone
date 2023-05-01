e80111e17afc1b432b79dbb3ce785c81b9448138
Syota Nishimura <syota4723@gmail.com>
2023-05-01 21:18:41 +0900

    :lipstick: Chatの送信部分のデザイン作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 import ChatHeader from './ChatHeader'
 import './Chat.scss'
 import './ChatHeader.scss'
+import { AddCircleOutline, CardGiftcard, EmojiEmotions, Gif } from '@mui/icons-material'
 
 const Chat = () => {
     return (
         <div className='chat'>
             <ChatHeader />
+            <div className="chatInput">
+                <AddCircleOutline />
+                <form>
+                    <input type="text" placeholder='Testへ送信' />
+                    <button type="submit" className='chatInputButton'></button>
+                </form>
+                <div className="chatInputIcons">
+                    <CardGiftcard />
+                    <Gif />
+                    <EmojiEmotions />
+                </div>
+            </div>
         </div>
     )
 }
