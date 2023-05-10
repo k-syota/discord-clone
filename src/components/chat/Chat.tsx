10087a40f93d9a3b4f0b2a745ce567646e898672
Syota Nishimura <syota4723@gmail.com>
2023-05-10 14:56:23 +0900

    :sparkles: リアルタイムでメッセージ情報を取得する

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatHeader from './ChatHeader'
 import ChatMessage from './ChatMessage'
 import './Chat.scss'
@@ -6,21 +6,50 @@ import './ChatHeader.scss'
 import './ChatMessage.scss'
 import { AddCircleOutline, CardGiftcard, EmojiEmotions, Gif } from '@mui/icons-material'
 import { useAppSelector } from '../../app/hooks'
-import { CollectionReference, DocumentData, addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { Timestamp, CollectionReference, DocumentData, addDoc, collection, onSnapshot, serverTimestamp } from 'firebase/firestore'
 import { db } from '../../firebase'
+
+interface Messages {
+    timestamp: Timestamp,
+    message: string,
+    user: {
+        uid: string;
+        photo: string;
+        email: string;
+        displayName: string;
+    }
+}
 
 const Chat = () => {
 
-    const [text, setText] = useState('')
+    const [text, setText] = useState<string>('')
+    const [messages, setMessages] = useState<Messages[]>([])
     const channelName = useAppSelector((state) => state.channel.channelName)
     const channelId = useAppSelector((state) => state.channel.channelId)
     const user = useAppSelector((state) => state.user.user)
+
+    useEffect(() => {
+        const collectionRef: CollectionReference<DocumentData> = collection(db, "channels", String(channelId), "messages")
+        onSnapshot(collectionRef, (snapshot) => {
+            let res: Messages[] = [];
+            snapshot.docs.forEach((doc) => {
+                res.push({
+                    timestamp: doc.data().timestamp,
+                    message: doc.data().message,
+                    user: doc.data().user
+                })
+            })
+            setMessages(res)
+        })
+    }, [channelId])
+
+    console.log(messages)
 
     const sendMessage = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         const collectionRef: CollectionReference<DocumentData> = collection(db, "channels", String(channelId), "messages")
         await addDoc(collectionRef, {
-            'messages': text,
+            'message': text,
             timestamp: serverTimestamp(),
             user: user
         })
